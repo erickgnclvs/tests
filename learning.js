b42d36b336aa8b3a04f643cf5306d52187f2a7da
erickgnclvs <erickgoncalves5@hotmail.com>
2022-11-18 19:21:11 -0300

    add js tests

--- learning.js
+++ learning.js
@@ -5,12 +5,26 @@ console.log("hello world");
 // Trip cost
 
 const gasPrice = 5.45;
+const alcoholPrice = 4.99;
+const nitrometanePrice = 51.5;
 const gasUsage = 7;
 const tripMileage = 600;
-
+const fuel = 'gas';
 const gasLitters = tripMileage / gasUsage;
 
-const result = gasLitters * gasPrice;
+if (fuel === 'alcohol') {
+    const result = gasLitters * alcoholPrice;
+    console.log(result.toFixed(2));
+} else if (fuel === 'gas') {
+    const result = gasLitters * gasPrice;
+    console.log(result.toFixed(2));
+} else {
+    const result = gasLitters * nitrometanePrice;
+    console.log(result.toFixed(2));
+}
 
-console.log(result.toFixed(2));
+
+
+
+
 
